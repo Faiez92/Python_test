44285190d54a61779017fb358773b5d8c6ee7d76
Mohammed Faiez ABID <faiez.abid@outlook.fr>
2020-08-06 18:57:08 +0100

    chemin function 2.0

--- sujet_3.py
+++ sujet_3.py
@@ -1,20 +1,49 @@
-							
-Plateau= [ [True , False , False , False ] ,
-                 [ False , True , True , False ] ]
+import numpy as np
 
-l=[]
+def compare_neighbors(arr):
+    comp_arr = np.full(arr.shape, False, dtype=bool) 
+    arr_height = arr.shape[0]
+    arr_width = arr.shape[1]
 
-def chemin(Plateau):
-	x = Plateau[1][3]
-	y = Plateau[0][1]
-	l.append(x)
-	l.append(y)
-	print(x)
-	print(l)
-	if True in l:
-	    print(True)
-	else:
-	    print(False)
+    for i in range(arr_height): 
+        for j in range(arr_width): 
 
-	
-chemin(Plateau)
+            center = arr[i,j]
+            if i == 0: #left 
+                left = arr[i,j]
+            else:
+                left = arr[i-1, j]
+
+            if i == arr_height - 1: #right
+                right = arr[i,j]
+            else:
+                right = arr[i+1,j]
+
+            if j == 0: #up
+                up = arr[i,j]
+            else:
+                up = arr[i, j-1]
+
+            if j == arr_width - 1: #down
+                down = arr[i,j]
+            else:
+                down = arr[i, j+1]
+
+            comp_arr[i,j] = len(set([left, right, up, down, center])) == 1
+            if arr[i,j]==False and comp_arr[i,j] == False:
+                if comp_arr[i,j+1] == True:
+                    return True
+                else:
+                    return False
+            elif arr[i,j]==True and comp_arr[i,j] == False:   
+                if comp_arr[i,j+1] == True:
+                    return True
+                else:
+                    return False
+    return comp_arr
+    
+Plateau = np.array ([ [True , False , False , False ] ,
+                 [ False , True , True , False ] ])
+                 
+compare_neighbors(Plateau)
+print(compare_neighbors(Plateau))
